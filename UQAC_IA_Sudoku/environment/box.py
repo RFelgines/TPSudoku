6cf96165531e3a7a7d88c4ecdea01b004e09516a
Romain <romain.felgines@gmail.com>
2022-02-20 12:22:51 -0500

    First commit

---
+++ UQAC_IA_Sudoku/environment/box.py
@@ -0,0 +1,15 @@
+class box:
+    number = None
+
+
+
+
+    def empty(self):
+        self.number = None
+
+
+    def setNumber(self, number):
+        self.number = number
+
+    def getNumber(self):
+        return self.number
